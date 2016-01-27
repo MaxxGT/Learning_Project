044433bb267124f74545ef407982ece8d4195b8b
MaxxGT <hau_sky@yahoo.com>
2016-01-27 20:28:10 +0800

    1

---
+++ careers.php
@@ -0,0 +1,104 @@
+<?php include('header.php'); ?>
+<!dotype html>
+<html>
+	<head>
+		<title>PBMART CARRERS</title>
+	</head>
+	<body>
+		<BR />
+		<table border='0' width='100%'>
+			<tr>
+				<td><center><img src='careers/careers-banner.jpg'></img></center></td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+			</tr>
+			<tr>
+				<td>
+					<a href="index.php">Home</a>-> Carrers-> Job Availability
+				</td>
+			</tr>
+		
+			<tr>
+				<td>&nbsp;</td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+			</tr>
+			<tr>
+				<td>
+					<font color='black' size='5'><B>SALESMAN</B></font>
+					<BR/>
+					<HR>
+				</td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+			</tr>
+			<tr>
+				<td>
+					<font color='black'>
+					- Must have Goods Drivers License(GDL).<BR/>
+					- Salary based on sales.<BR/>
+					- 18 years and above.
+					</font>
+				</td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+			</tr>
+			<tr>
+				<td>
+					<font color='black' size='5'><B>MARKETING MANAGER</B></font>
+					<BR/>
+					<HR>
+				</td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+			</tr>
+			<tr>
+				<td>
+					<font color='black'>
+					- Minimum 2 years working experience in marketing.<BR/>
+					- Salary based on experience.<BR/>
+					- 18 years and above.
+					</font>
+				</td>
+			</tr>
+		
+			<tr>
+				<td>&nbsp;</td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+			</tr>
+			<tr>
+				<td><font color='black' size='3'><B>If interested, please call us during office hours at 082-688968 or email us at pbmartadmin@pbmart.com.my</B></font></td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+			</tr>
+			<tr>
+				<td>
+					<?php include('footer.php'); ?>
+				</td>
+			</tr>
+		</table>
+	</body>
+</html>
