41286a6e5cb63dd64fa339ed35c07a34ededa3aa
MaxxGT <hau_sky@yahoo.com>
2016-01-27 20:37:58 +0800

    done deletion

--- commercial_pdf.php
+++ commercial_pdf.php
@@ -27,6 +27,5 @@ header('Content-Disposition: inline; filename="' . $filename . '"');
 header('Content-Transfer-Encoding: binary');
 header('Content-Length: ' . filesize($file));
 header('Accept-Ranges: bytes');
-
 @readfile($file);
 ?>
